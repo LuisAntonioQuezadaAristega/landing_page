10446c9b9c87c102d7f8cf3d9e0c67dd63c9610e
Luis <luquaris@espol.edu.ec>
2024-06-03 10:57:01 -0500

    se creo main.js

---
+++ furni-1.0.0/scripts/main.js
@@ -0,0 +1,10 @@
+let loaded = (eventLoaded) => {
+    let myform = document.getElementById('formulario');
+    myform.addEventListener('submit', ( eventSubmit ) => { 
+
+        debugger;
+    
+    })
+}
+
+window.addEventListener("DOMContentLoaded", loaded);
