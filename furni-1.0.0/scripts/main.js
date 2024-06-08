baf7164a079394442cbaf38aae18e583a301e2ba
LuisAntonioQuezadaAristega <107262704+LuisAntonioQuezadaAristega@users.noreply.github.com>
2024-06-08 16:53:10 -0500

    se actualizo el formulario y main.js

--- furni-1.0.0/scripts/main.js
+++ furni-1.0.0/scripts/main.js
@@ -2,6 +2,29 @@ let loaded = (eventLoaded) => {
     let myform = document.getElementById('formulario');
     myform.addEventListener('submit', ( eventSubmit ) => { 
 
+        eventSubmit.preventDefault();
+
+        let nombreValue = nombre.value;
+        let correoValue = correo.value;
+    
+        if( nombreValue.length == 0 ) {
+          
+          nombre.focus()
+    
+          alert('Ingrese un nombre válido')
+    
+          return;
+        }
+
+        if( correoValue.length == 0 ) {
+          
+            correo.focus()
+      
+          alert('Ingrese un correo válido')
+      
+          return;
+        }
+        
         debugger;
     
     })
