b9fe9ef55497f0e696d185ecf8ce830e3e930add
LuisAntonioQuezadaAristega <107262704+LuisAntonioQuezadaAristega@users.noreply.github.com>
2024-06-10 10:56:31 -0500

    actualizacion

--- furni-1.0.0/scripts/main.js
+++ furni-1.0.0/scripts/main.js
@@ -3,8 +3,12 @@ let loaded = (eventLoaded) => {
     myform.addEventListener('submit', ( eventSubmit ) => { 
 
         eventSubmit.preventDefault();
-    
-        if( nombre.value.length == 0 ) {
+		
+		const nombre = document.getElementById('nombre').value;
+		const correo = document.getElementById('correo').value;
+		const consola = document.getElementById('consola').value;
+
+        if( nombre.length == 0 ) {
           
           nombre.focus()
     
@@ -13,7 +17,7 @@ let loaded = (eventLoaded) => {
           return;
         }
 
-        if( correo.value.length == 0 ) {
+        if( correo.length == 0 ) {
           
             correo.focus()
       
@@ -22,32 +26,60 @@ let loaded = (eventLoaded) => {
           return;
         }
         
-        const formulario = document.getElementById('formulario');
-		    formulario.addEventListener('submit',(event)=>{
-			    event.preventDefault(); const nombre = document.getElementById('nombre').value;
-			    const correo = document.getElementById('correo').value;
-			    const tipo = document.getElementById('tipo').value;
-			    const datos = {
-				    nombre:nombre,
-				    correo:correo, 
-				    tipo:tipo
-			    };
-			    fetch('https://proyecto02-49e35-default-rtdb.firebaseio.com/collection.json', {
-				    method: 'POST',
-				    body: JSON.stringify(datos),
-				    headers: {
-				    	'Content-Type': 'application/json'
-				    }
-			    })
-			    .then(respuesta => respuesta.json())
-			    .then(datos => {
-				    console.log(datos); // Imprimir la respuesta del servidor
-				  })
-				  .catch(error => console.error(error));
-		    });
+		const datos = {
+			nombre:nombre,
+			correo:correo, 
+			consola:consola
+		};
+		fetch('https://proyecto02-49e35-default-rtdb.firebaseio.com/collection.json', {
+			method: 'POST',
+			body: JSON.stringify(datos),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		.then(respuesta => respuesta.json())
+		.then(datos => {
+			alert("tu respuesta ha sido guardada!")
+			//console.log(datos); // Imprimir la respuesta del servidor
+		})
+		.catch(error => console.error(error));
+		
         debugger;
-    
     })
 }
 
 window.addEventListener("DOMContentLoaded", loaded);
+obtenerDatos();
+async function obtenerDatos() {
+	const url = 'https://proyecto02-49e35-default-rtdb.firebaseio.com/collection.json';
+	const respuesta = await fetch(url);
+	if (!respuesta.ok) {
+		console.error("Error:", respuesta.status);
+		return;
+	}
+	const datos = await respuesta.json();
+	let votesMap = new Map();
+	for(const key in datos){
+		let votes = datos[key]
+		let consola = votes['consola']
+
+		let conteo = votesMap.has(consola)?votesMap.get(consola)+1:1;
+		votesMap.set(consola, conteo)
+	}
+
+	total = 0
+	tablebody.innerHTML = ''
+
+	console.log(votesMap);
+
+	/*for(let key of votesMap.keys()){
+		template = '
+			<tr>
+				<td>${categoria}</td>
+				<td>${conteo}</td>
+			</tr>
+			'
+	}*/
+
+}
